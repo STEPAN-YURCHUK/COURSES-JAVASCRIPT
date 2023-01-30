9ab9d0cf1beb75a3f2085f2d4c41586d57216aad
Степан Юрчук <yurchuk@MacBook-Air-Stepan.local>
2023-01-31 00:39:32 +0200

    done task 1

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,13 +1,13 @@
-let money = prompt('Ваш бюджет на месяц?', ''),
+let money = +prompt('Ваш бюджет на месяц?', ''),
     time = prompt('Введите дату в формате YYYY-MM-DD', '')
 
 let appData = {
-    "money": money,
-    "timeData": time,
+    money: money,
+    timeData: time,
     expenses: {},
-    "optionalExpenses": {},
-    "income": [],
-    "savings": false
+    optionalExpenses: {},
+    income: [],
+    savings: false
 }
 
 let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
