fc58c28fdd0aabd4147064daf690b0f154a2b8a3
Степан Юрчук <yurchuk@MacBook-Air-Stepan.local>
2023-02-06 19:40:09 +0200

    done tast 8

--- TEST/script.js
+++ TEST/script.js
@@ -1,32 +1,67 @@
-let btn = document.querySelector('.btn'),
-    elem = document.querySelector('.box');
+// function User(name, id) {
 
-function myAnimation() {
-    let pos = 0;
-    
-    let id = setInterval(frame, 3);
-    function frame() {
-        if (pos <= 99) {
-            pos++;
-            elem.style.top = pos + 'px';  
-        } else if (pos >= 100 && pos <= 199){
-            pos++;
-            elem.style.left = (pos - 100) + 'px';
-        } else if (pos >= 200 && pos <= 299){
-            pos++;
-            elem.style.top = 100 - (pos - 200) + 'px';
-        } else if (pos >= 300 && pos <= 399){
-            pos++;
-            elem.style.left = 100 - (pos - 300) + 'px';
-        } else {
-            clearInterval(id);
-            
-        }
-    }
-    
-}
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//     console.log('Hello! ' + this.name);
+//     }
+// }
+// User.prototype.exit = function(name) {
+//     console.log('Пользователь ' + this.name + ' ушел');
+// }
 
-btn.addEventListener('click', myAnimation);
 
-btn.addEventListener('touchstart', myAnimation);
+// let ivan = new User('Ivan', 25),
+//     alex = new User('Alex', 20);
 
+// console.log(ivan); // User {name: "Ivan", id: 25, human: true}
+// console.log(alex); // User {name: "Alex", id: 20, human: true}
+
+// ivan.hello() // Hello! Ivan
+// alex.hello() // Hello! Alex
+
+// ivan.exit() // Пользователь Ivan ушел
+
+// console.log(User);
+
+// 'use strict';
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+//     console.log(sum());
+// }
+// showThis(4, 5);
+// showThis(5, 5);
+
+
+
+// let user = {
+//     name: "Jhon",
+// };
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// console.log(sayName.call(user, 'Smith')); // Call принимает в виде строки
+// console.log(sayName.apply(user), ['Snow']); // Apply принимает в виде массива
+
+// function count(number) {
+//     return this * number;
+// }
+
+// let double = count.bind(2);
+
+// console.log(double[3]);
+// console.log(double[10]);
+
+
+// 1) Просто вызов функции - window/undefiend
+// 2) Метод объекта - this = объект
+// 3) Конструктор (new) - this = новый созданный объект
+// 4) Указание конкретного контекста - call, apply, bind
